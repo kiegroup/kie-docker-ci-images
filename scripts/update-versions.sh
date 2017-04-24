7e7016d36c60130ce08bd6d58bcaa93caea1c869
Petr Široký <psiroky@redhat.com>
2017-04-24 13:48:10 +0200

    Fix update versions script

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -11,6 +11,5 @@ fi
 
 KIE_VERSION=$1
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-# extract timestamp from the version string. This assumes the version is in format <6-chars><timestamp>, for example
-# version 6.4.0.20150922-142344 will result in timestamp "20150922-142344"
-TIMESTAMP=`echo ${KIE_VERSION} | cut -c 7-`
+cd ${SCRIPTS_DIR}/..
+mvn -N -B -e versions:set -DnewVersion=${KIE_VERSION} -DallowSnapshots=true -DgenerateBackupPoms=false
