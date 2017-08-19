a9d523abe72fd3a3c15fa372dd8e7a9893090419
Petr Široký <psiroky@redhat.com>
2017-08-19 12:23:44 +0200

    Better handling of additional maven args

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -5,15 +5,18 @@ set -e
 # 1. - The KIE version to use
 # 2. - Additional Maven arguments (optional)
 
-if [ $# -eq 1 ]; then
+if [ $# -ge 1 ]; then
     KIE_VERSION=$1
-elif [ $# -eq 2 ]; then
-    KIE_VERSION=$1
-    ADDITIONAL_MAVEN_ARGS=$2
+    shift
+    # everything after the first param is treated as additional maven args
+    ADDITIONAL_MAVEN_ARGS="$@"
 else
-    echo "[ERROR] Incorrect number of params! KIE Version needs to be specified as first param and (optionally) additional Maven arguments as second param. Exiting!"
+    echo "[ERROR] Incorrect number of params! KIE Version needs to be specified as first param and (optionally) additional Maven arguments as the other params. Exiting!"
     exit 65
 fi
+
+echo "KIE version: $KIE_VERSION"
+echo "Additional Maven args: ${ADDITIONAL_MAVEN_ARGS[@]}"
 
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # extract timestamp from the version string. This assumes the version is in format <6-chars><timestamp>, for example
@@ -21,10 +24,15 @@ SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TIMESTAMP=`echo ${KIE_VERSION} | cut -c 7-`
 
 cd ${SCRIPTS_DIR}/..
-DASHBUILDER_VERSION=`mvn -B -e ${ADDITIONAL_MAVEN_ARGS} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=docker.dashbuilder.artifacts.version | grep -v '\['`
+
+# don't use versions:update-parent to update the parent version as that is buggy and often does not update the version for some reason
+PARENT_VERSION=`mvn -B -e ${ADDITIONAL_MAVEN_ARGS[@]} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.parent.version | grep -v '\['`
+echo "Parsed parent version: $PARENT_VERSION"
+sed -i "s/<version>$PARENT_VERSION<\/version>/<version>$KIE_VERSION<\/version>/" pom.xml
+mvn -B -N -e ${ADDITIONAL_MAVEN_ARGS[@]} versions:update-child-modules -DallowSnapshots=true -DgenerateBackupPoms=false
+
+DASHBUILDER_VERSION=`mvn -B -e ${ADDITIONAL_MAVEN_ARGS[@]} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=docker.dashbuilder.artifacts.version | grep -v '\['`
 DASHBUILDER_BASE_VERSION=`echo ${DASHBUILDER_VERSION} | cut -c -5`
 echo "Dashbuilder version: $DASHBUILDER_VERSION"
 echo "Base Dashbuilder version: $DASHBUILDER_BASE_VERSION"
-mvn -U -B -N -e ${ADDITIONAL_MAVEN_ARGS} versions:update-parent -DparentVersion=[${KIE_VERSION}] -DallowSnapshots=true -DgenerateBackupPoms=false
-mvn -B -N -e ${ADDITIONAL_MAVEN_ARGS} versions:update-child-modules -DallowSnapshots=true -DgenerateBackupPoms=false
 sed -i "s/<docker\.build\.dashbuilder\.tag>.*<\/docker\.build\.dashbuilder\.tag>/<docker\.build\.dashbuilder\.tag>${DASHBUILDER_BASE_VERSION}.${TIMESTAMP}<\/docker\.build\.dashbuilder\.tag>/" pom.xml
