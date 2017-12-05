2fb4c39859f5b09cd4e3371ad47084ab344eac81
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2017-12-05 20:20:55 +0100

    updated to 7.5.x

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -20,7 +20,7 @@ echo "Additional Maven args: ${ADDITIONAL_MAVEN_ARGS[@]}"
 
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # extract timestamp from the version string. This assumes the version is in format <6-chars><timestamp>, for example
-# version 6.4.0.20150922-142344 will result in timestamp "20150922-142344"
+# version 7.5.0.20170922-142344 will result in timestamp "20170922-142344"
 TIMESTAMP=`echo ${KIE_VERSION} | cut -c 7-`
 
 cd ${SCRIPTS_DIR}/..
@@ -30,9 +30,3 @@ PARENT_VERSION=`mvn -B -e ${ADDITIONAL_MAVEN_ARGS[@]} org.apache.maven.plugins:m
 echo "Parsed parent version: $PARENT_VERSION"
 sed -i "s/<version>$PARENT_VERSION<\/version>/<version>$KIE_VERSION<\/version>/" pom.xml
 mvn -B -N -e ${ADDITIONAL_MAVEN_ARGS[@]} versions:update-child-modules -DallowSnapshots=true -DgenerateBackupPoms=false
-
-DASHBUILDER_VERSION=`mvn -B -e ${ADDITIONAL_MAVEN_ARGS[@]} org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=docker.dashbuilder.artifacts.version | grep -v '\['`
-DASHBUILDER_BASE_VERSION=`echo ${DASHBUILDER_VERSION} | cut -c -5`
-echo "Dashbuilder version: $DASHBUILDER_VERSION"
-echo "Base Dashbuilder version: $DASHBUILDER_BASE_VERSION"
-sed -i "s/<docker\.build\.dashbuilder\.tag>.*<\/docker\.build\.dashbuilder\.tag>/<docker\.build\.dashbuilder\.tag>${DASHBUILDER_BASE_VERSION}.${TIMESTAMP}<\/docker\.build\.dashbuilder\.tag>/" pom.xml
