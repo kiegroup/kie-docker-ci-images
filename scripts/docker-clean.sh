049cb70d7e52d5f93883f5131f47e752304b3b65
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2017-12-06 10:46:22 +0100

    updated to 7.6.0-SNAPSHOT (#18)

--- scripts/docker-clean.sh
+++ scripts/docker-clean.sh
@@ -17,26 +17,20 @@ fi
 KIE_VERSION=$1
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # extract timestamp from the version string. This assumes the version is in format <6-chars><timestamp>, for example
-# version 6.4.0.20150922-142344 will result in timestamp "20150922-142344"
+# version 7.6.0.20180922-142344 will result in timestamp "20180922-142344"
 BASE_VERSION=`echo ${KIE_VERSION} | cut -c -5`
 TIMESTAMP=`echo ${KIE_VERSION} | cut -c 7-`
 cd ${SCRIPTS_DIR}/..
-DASHBUILDER_VERSION=`mvn -B -e org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=docker.dashbuilder.artifacts.version | grep -v '\['`
-DASHBUILDER_BASE_VERSION=`echo ${DASHBUILDER_VERSION} | cut -c -5`
 
 echo "Stopping containers with ${TIMESTAMP} in name"
 docker ps | grep "${TIMESTAMP//./\\.}" | awk '{print $1}' | xargs -r docker stop
 echo "Stopping containers with ${BASE_VERSION} in name"
 docker ps | grep "${BASE_VERSION//./\\.}" | awk '{print $1}' | xargs -r docker stop
-echo "Stopping containers with ${DASHBUILDER_BASE_VERSION} in name"
-docker ps | grep "${DASHBUILDER_BASE_VERSION//./\\.}" | awk '{print $1}' | xargs -r docker stop
 
 echo "Removing containers with ${TIMESTAMP} in name"
 docker ps -a | grep "${TIMESTAMP//./\\.}" | awk '{print $1}' | xargs -r docker rm -f
 echo "Removing containers with ${BASE_VERSION} in name"
 docker ps -a | grep "${BASE_VERSION//./\\.}" | awk '{print $1}' | xargs -r docker rm -f
-echo "Removing containers with ${DASHBUILDER_BASE_VERSION} in name"
-docker ps -a | grep "${DASHBUILDER_BASE_VERSION//./\\.}" | awk '{print $1}' | xargs -r docker rm -f
 
 # don't remove images for the base version, they should be removed periodically with other script
 # we want to keep them around for some days in case needed
